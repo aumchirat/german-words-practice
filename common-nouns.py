fbf63d9733b8a8af6a0bc62adbd212150898ebd8
chirat_n <148340870+aumchirat@users.noreply.github.com>
2025-05-12 15:28:47 +0700

    WIP

--- common-nouns.py
+++ common-nouns.py
@@ -1,28 +1,37 @@
 import pandas as pd
 import random
+import time
 
 nouns_df = pd.read_excel("./data/most-used-german-words.xlsx", sheet_name='nouns')
 noun_data = nouns_df.set_index('german')['english'].to_dict()
-# for key, value in noun_data.items():
-#     print(key, ':', value)
 
-# let user choose how many words they'd like to practice
-num_words = 2
-pracetice_words = []
-# random key and value
-for i in range(num_words):
-    random_word = random.choice(list(noun_data.items()))
-    ger = random_word[0]
-    eng = random_word[1]
-    print(ger + ': ')
-    answer = input()
-    if answer == eng:
-        print('CORRECT!')
-    else:
-        print('That is not correct, save this word for later? (Y/N)')
-        save_word = input()
-        if save_word == 'Y':
+def practice_nouns():
+    print("How many words would you like to practice?")
+    num_words = input()
+    pracetice_words = []
+    # random key and value
+    for i in range(int(num_words)):
+        random_word = random.choice(list(noun_data.items()))
+        ger = random_word[0]
+        eng = random_word[1]
+        print(ger + ': ')
+        answer = input()
+        if answer == eng:
+            print('CORRECT!')
+        else:
+            print('That is not correct, the correct answer is ' + eng)
             pracetice_words.append(ger)
+    print("\n..............")
+    print("These are words you should learn more: ")
+    for word in pracetice_words:
+        print(word + " ... means => " + noun_data[word])
+    print("\n..............")
 
-
-print(pracetice_words)
+    print("Want to keep practicing words you don't know? (Y/N)")
+    keep_practicing = input()
+    if keep_practicing == "Y":
+        practice_nouns()
+    else:
+        print("ok! schüss..")
+   
+practice_nouns()
