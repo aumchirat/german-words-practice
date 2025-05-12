cbd973aab4ae3b96a66394daab471a9b4c6e4489
chirat_n <148340870+aumchirat@users.noreply.github.com>
2025-05-12 16:08:48 +0700

    WIP

--- common-nouns.py
+++ common-nouns.py
@@ -3,35 +3,37 @@ import random
 import time
 
 nouns_df = pd.read_excel("./data/most-used-german-words.xlsx", sheet_name='nouns')
-noun_data = nouns_df.set_index('german')['english'].to_dict()
+# noun_data = nouns_df.set_index('german')['english'].to_dict()
+noun_data = nouns_df.to_dict('records')
+# any english definition with "|"
 
-def practice_nouns():
-    print("How many words would you like to practice?")
+
+def practice_nouns(practice_set=noun_data):
+    print("How many words would you like to practice? (from 1 to " + str(len(practice_set)) + ")")
     num_words = input()
     pracetice_words = []
     # random key and value
     for i in range(int(num_words)):
-        random_word = random.choice(list(noun_data.items()))
-        ger = random_word[0]
-        eng = random_word[1]
-        print(ger + ': ')
+        random_num = random.randint(0, len(practice_set) - 1)
+        ger = practice_set[random_num]["german"]
+        eng_list = practice_set[random_num]["english"].split("|")
+        print(ger + ": ")
         answer = input()
-        if answer == eng:
+        if answer in eng_list:
             print('CORRECT!')
         else:
-            print('That is not correct, the correct answer is ' + eng)
-            pracetice_words.append(ger)
-    print("\n..............")
-    print("These are words you should learn more: ")
-    for word in pracetice_words:
-        print(word + " ... means => " + noun_data[word])
-    print("\n..............")
-
-    print("Want to keep practicing words you don't know? (Y/N)")
-    keep_practicing = input()
-    if keep_practicing == "Y":
-        practice_nouns()
-    else:
-        print("ok! schüss..")
+            print('That is not correct, the correct answer is ' + practice_set[random_num]["english"])
+            pracetice_words.append(practice_set[random_num])
+    # if there're still words to learn
+    if len(pracetice_words) > 0:
+        print("words you don't know: ")
+        for word in pracetice_words:
+            print(word["german"] + "   =>  " + word["english"])
+        print("Want to keep practicing words you don't know? (Y/N)")
+        keep_practicing = input()
+        if keep_practicing == "Y":
+            practice_nouns(pracetice_words)
+        else:
+            print("ok! schüss..")
    
 practice_nouns()
